771a8d03626c76996db24608af3d685d80680d67
natan-alex <natancarmo321@gmail.com>
2022-06-19 23:08:19 -0300

    adjust tiles consts file

---
+++ src/consts/tiles.rs
@@ -0,0 +1,9 @@
+use phf::{phf_map};
+
+use crate::domain::tile::tile_kind::TileKind;
+
+pub static SYMBOLS_AND_KINDS: phf::Map<char, TileKind> = phf_map! {
+    '|' => TileKind::Blocked('|'),
+    '*' => TileKind::PowerUp('*'),
+    '.' => TileKind::PointObject('.'),
+};
